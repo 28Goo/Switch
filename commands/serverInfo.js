902ce1cb586721b27c932c17196415b159714b94
28Goo <brillantes.git@gmail.com>
2021-10-10 18:12:45 +0800

    Slash commands and embed added

---
+++ commands/serverInfo.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Displays server\'s info.'),
+	async execute(interaction) {
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle(`${interaction.guild.name}`)
+			.addFields(
+				{ name: 'Members', value: `${interaction.guild.memberCount}` },
+			)
+			.setTimestamp()
+			.setFooter('Footer text');
+		await interaction.reply({ embeds: [embed] });
+	},
+};
--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // Get requirements for bot to work
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents, MessageActionRow, MessageButton } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	// Get file
+	const command = require(`./commands/${file}`);
+
+	// Get command from file and add to collection
+	client.commands.set(command.data.name, command);
+}
+
 // When client is ready, run code below
-client.once('ready', () => {
-	console.log('Ready!');
+client.once('ready', c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing the command.', ephemeral:true });
+	}
 });
 
 client.login(token);
