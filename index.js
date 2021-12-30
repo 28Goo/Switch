57cc13ea1407bcaecfa7c0d53c13064d2468b85e
28Goo <brillantes.git@gmail.com>
2021-12-30 19:19:03 +0800

    presence updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ for (const file of commandFiles) {
 // When client is ready, run code below
 client.once('ready', c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
-	c.user.setPresence({ activities: [{ name: '/play', type:'LISTENING' }] });
+	c.user.setPresence({ activities: [{ name: '/help', type:'LISTENING' }] });
 });
 
 client.on('interactionCreate', async interaction => {
